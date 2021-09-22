8489b2e916bdc41f6b625c3e5b50323a60be5397
Sridevi Muralikanthan <90853298+sridevi-mk@users.noreply.github.com>
2021-09-22 11:01:34 -0400

    Create fact.py

---
+++ fact.py
@@ -0,0 +1,13 @@
+def factorial(a):
+    if a==1:
+        return a
+    else:
+        return a*factorial(a-1)
+
+a=int(input("Enter a number : "))
+if a<0:
+    print("There is no factorial for 0.")
+elif a == 0:
+    print("The factorial of 0 is 1.")
+else:
+    print("The factorial of",a,"is",str(factorial(a))+".")
